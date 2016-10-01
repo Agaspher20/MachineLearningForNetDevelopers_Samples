a50838ba35761a17523e0bd5755463ff0c1f6c33
Michael Naumenko <agaspher@outlook.com>
2016-10-01 13:46:10 +0300

    Count of bikes rented charts

--- BikesAndMen/BikesAndMen/Script.fsx
+++ BikesAndMen/BikesAndMen/Script.fsx
@@ -8,3 +8,20 @@ let dataPath = __SOURCE_DIRECTORY__ + @"..\..\data\day.csv"
 type Data = CsvProvider<dataPath>
 let dataSet = Data.Load(dataPath)
 let data = dataSet.Rows
+
+#load @"FSharp.Charting.0.90.14\FSharp.Charting.fsx"
+open FSharp.Charting
+
+Chart.Line [ for obs in data -> obs.Cnt ]
+
+let ma n (series:float seq) =
+    series
+    |> Seq.windowed n
+    |> Seq.map (fun xs -> xs |> Seq.average)
+    |> Seq.toList
+
+let count = seq { for obs in data -> float(obs.Cnt) }
+Chart.Combine [
+    Chart.Line count
+    Chart.Line (ma 7 count)
+    Chart.Line (ma 30 count) ]
