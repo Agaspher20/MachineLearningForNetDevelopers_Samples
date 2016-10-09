7af5152ee7bd070117354d4a13039b8b99c953c6
Michael Naumenko <agaspher@outlook.com>
2016-10-09 12:35:56 +0300

    Linear batch gradient descent predictor

--- BikesAndMen/BikesAndMen/LinearBatchGradientDescent.fsx
+++ BikesAndMen/BikesAndMen/LinearBatchGradientDescent.fsx
@@ -43,3 +43,81 @@ cost theta Y X
 let estimate (Y:Vec) (X:Mat) =
     (X.Transpose() * X).Inverse() * X.Transpose() * Y
 
+let seed = 314159
+let rng = System.Random(seed)
+
+let shuffle (arr:'a []) =
+    let arr = Array.copy arr
+    let l = arr.Length
+    for i in (l-1) .. -1 .. 1 do
+        let temp = arr.[i]
+        let j = rng.Next(0,i+1)
+        arr.[i] <- arr.[j]
+        arr.[j] <- temp
+    arr
+
+let training,validation =
+    let shuffled =
+        data
+        |> Seq.toArray
+        |> shuffle
+    let size =
+        0.7 * float (Array.length shuffled)
+        |> int
+    shuffled.[..size],
+    shuffled.[size+1..]
+
+type Obs = Data.Row
+type Model = Obs -> float
+type Featurizer = Obs -> float list
+
+let predictor (f:Featurizer) (theta:Vec) =
+    f >> vector >> (*) theta
+
+let evaluate (model:Model) (data:Obs seq) =
+    data
+    |> Seq.averageBy (fun obs ->
+        abs (model obs - float obs.Cnt))
+
+let model (f:Featurizer) (data:Obs seq) =
+    let Yt, Xt =
+        data
+        |> Seq.toList
+        |> List.map (fun obs -> float obs.Cnt, f obs)
+        |> List.unzip
+    let theta = estimate (vector Yt) (matrix Xt)
+    let predict = predictor f theta
+    theta,predict
+
+let featurizer0 (obs:Obs) =
+    [   1.;
+        float obs.Instant; ]
+
+let (theta0,model0) = model featurizer0 training
+
+evaluate model0 training |> printfn "Training: %.0f"
+evaluate model0 validation |> printfn "Validation: %.0f" 
+
+Chart.Combine [
+    Chart.Line [ for obs in data -> float obs.Cnt ]
+    Chart.Line [ for obs in data -> model0 obs ] ]
+
+
+let featurizer1 (obs:Obs) =
+    [   1.
+        obs.Instant |> float
+        obs.Atemp |> float
+        obs.Hum |> float
+        obs.Temp |> float
+        obs.Windspeed |> float
+    ] 
+let (theta1,model1) = model featurizer1 training 
+
+evaluate model1 training |> printfn "Training: %.0f"
+evaluate model1 validation |> printfn "Validation: %.0f" 
+
+Chart.Point [ for obs in data -> float obs.Cnt, model1 obs ]
+
+Chart.Combine [
+    Chart.Line [ for obs in data -> float obs.Cnt ]
+    Chart.Line [ for obs in data -> model1 obs ] ]
