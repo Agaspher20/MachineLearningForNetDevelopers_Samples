2fc6ad59e1e808688f3e6940ebf61a4967eb81c3
Michael Naumenko <agaspher@outlook.com>
2016-10-25 21:42:03 +0300

    Tags recommender

---
+++ YouAreNotAUniqueSnowflake/YouAreNotAUniqueSnowflake/TagsRecommender.fsx
@@ -0,0 +1,93 @@
+﻿#I @"..\packages\"
+
+open System
+open System.IO
+
+let generalPath = __SOURCE_DIRECTORY__ + @"..\..\data\";
+let file = "userprofiles-toptags.txt"
+let headers,observations = 
+    let raw =
+        generalPath + file
+        |> File.ReadAllLines 
+    // first row is headers, first col is user ID
+    let headers = (raw.[0].Split ',').[1..] 
+    let observations =
+        raw.[1..]
+        |> Array.map (fun line -> (line.Split ',').[1..])
+        |> Array.map (Array.map float)
+        |> Array.filter (fun x -> Array.sum x > 0.)
+    headers,observations
+
+let scale (row:float[]) =
+    let min = row |> Array.min
+    let max = row |> Array.max
+    if min = max
+        then row
+        else
+            row |> Array.map (fun x -> (x - min) / (max - min))
+
+let test = observations.[..99] |> Array.map scale
+let train = observations.[100..] |> Array.map scale
+
+let distance (row1:float[]) (row2:float[]) =
+    (row1,row2)
+    ||> Array.map2 (fun x y -> pown (x - y) 2)
+    |> Array.sum 
+
+let similarity (row1:float[]) (row2:float[]) =
+    1. / (1. + distance row1 row2) 
+
+let split (row:float[]) =
+    row.[..19],row.[20..] 
+
+let weights (values:float[]) =
+    let total = values |> Array.sum
+    values |> Array.map (fun x -> x / total)
+
+let predict (row:float[]) =
+    let known,unknown = row |> split
+    let similarities =
+        train
+        |> Array.map (fun example ->
+            let common, _ = example |> split
+            similarity known common)
+        |> weights
+    [| for i in 20 .. 29 ->
+        let column = train |> Array.map (fun x -> x.[i])
+        let prediction =
+            (similarities,column)
+            ||> Array.map2 (fun s v -> s * v)
+            |> Array.sum
+        prediction |]
+
+let targetTags = headers.[20..] 
+predict test.[0] |> Array.zip targetTags
+
+test.[0] |> split |> snd
+
+let validation =
+    test
+    |> Array.map (fun obs ->
+        let actual = obs |> split |> snd
+        let predicted = obs |> predict
+        let recommended, observed =
+            Array.zip predicted actual
+            |> Array.maxBy fst
+        if observed > 0. then 1. else 0.)
+    |> Array.average
+    |> printfn "Correct calls: %f"
+
+let averages =  [|
+    for i in 20 .. 29 ->
+        train |> Array.averageBy(fun row -> row.[i]) |]
+let baseline =
+    test
+    |> Array.map (fun obs ->
+        let actual = obs |> split |> snd
+        let predicted = averages
+        let recommended, observed =
+            Array.zip predicted actual
+            |> Array.maxBy fst
+        if observed > 0. then 1. else 0.)
+    |> Array.average
+    |> printfn "Correct calls: %f"
