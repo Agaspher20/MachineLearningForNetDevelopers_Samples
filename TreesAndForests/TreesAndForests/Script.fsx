468812faef2dfbfd28d704a663e58ba420c708f7
Michael Naumenko <agaspher@outlook.com>
2016-10-29 12:06:01 +0300

    Data research

--- TreesAndForests/TreesAndForests/Script.fsx
+++ TreesAndForests/TreesAndForests/Script.fsx
@@ -2,3 +2,42 @@
 #r @"FSharp.Data.2.3.2\lib\net40\FSharp.Data.dll"
 
 open FSharp.Data
+
+[<Literal>]
+let dataPath = __SOURCE_DIRECTORY__ + @"..\..\data\titanic.csv"
+
+type Titanic = CsvProvider<dataPath>
+type Passenger = Titanic.Row
+
+let dataset = Titanic.GetSample ()
+
+dataset.Rows
+|> Seq.countBy (fun passenger -> passenger.Survived)
+|> Seq.iter (printfn "%A")
+
+dataset.Rows
+|> Seq.averageBy (fun passenger ->
+    if passenger.Survived then 1.0 else 0.0)
+|> printfn "Chances of survival: %.3f"
+
+let survivalRate (passengers:Passenger seq) =
+    let total = passengers |> Seq.length
+    let survivors =
+        passengers
+        |> Seq.filter (fun p -> p.Survived)
+        |> Seq.length
+    100.0 * (float survivors / float total)
+
+let bySex =
+    dataset.Rows
+    |> Seq.groupBy(fun p -> p.Sex)
+
+bySex |> Seq.iter (fun (s,g) ->
+    printfn "Sex %A: %f" s (survivalRate g))
+
+let byClass =
+    dataset.Rows
+    |> Seq.groupBy (fun p -> p.Pclass)
+
+byClass |> Seq.iter (fun (s,g) ->
+    printfn "Class %A: %f" s (survivalRate g))
