462ca0358b0cc490fc76a675c24ef0a2f4ae9edf
Michael Naumenko <agaspher@outlook.com>
2016-10-30 13:07:14 +0300

    Making predictions with decision stump

---
+++ TreesAndForests/TreesAndForests/DecisionStump.fs
@@ -0,0 +1,52 @@
+﻿module DecisionStump
+
+    let mostFrequentLabelIn group =
+        group
+        |> Seq.countBy snd
+        |> Seq.maxBy snd
+        |> fst
+
+    let hasData extractFeature = extractFeature >> Option.isSome
+    
+    let learn sample extractFeature extractLabel =
+        // group together observations that have the
+        // same value for the selected feature, and
+        // find the most frequent label by group.
+        let groups =
+            sample
+            |> Seq.map (fun obs -> extractFeature obs, extractLabel obs)
+            |> Seq.groupBy fst
+            |> Seq.map (fun (feat,group) -> feat, mostFrequentLabelIn group)
+        // for an observation, find the group with
+        // matching feature value, and predict the
+        // most frequent label for that group.
+        let classifier obs =
+            let featureValue = extractFeature obs
+            groups
+            |> Seq.find (fun (f,_) -> f = featureValue)
+            |> snd
+
+        classifier
+
+    let betterLearn sample extractFeature extractLabel =
+        let branches =
+            sample
+            |> Seq.filter (extractFeature |> hasData)
+            |> Seq.map (fun obs -> extractFeature obs |> Option.get, extractLabel obs)
+            |> Seq.groupBy fst
+            |> Seq.map (fun (feat,group) -> feat, mostFrequentLabelIn group)
+            |> Map.ofSeq
+        let labelForMissingValues =
+            sample
+            |> Seq.countBy extractLabel
+            |> Seq.maxBy snd
+            |> fst
+        let classifier obs =
+            let featureValue = extractFeature obs
+            match featureValue with
+            | None -> labelForMissingValues
+            | Some(value) ->
+                match (branches.TryFind value) with
+                | None -> labelForMissingValues
+                | Some(predictedLabel) -> predictedLabel
+        classifier
--- TreesAndForests/TreesAndForests/Script.fsx
+++ TreesAndForests/TreesAndForests/Script.fsx
@@ -41,3 +41,52 @@ let byClass =
 
 byClass |> Seq.iter (fun (s,g) ->
     printfn "Class %A: %f" s (survivalRate g))
+
+#load "DecisionStump.fs"
+open DecisionStump
+
+let survived (p:Passenger) = p.Survived
+let validate (featureName:string) classifier (rows:seq<Passenger>) =
+    printfn "Stump: classify based on %s." featureName
+    rows
+    |> Seq.averageBy (fun p ->
+        if p.Survived = classifier p then 1.0 else 0.0)
+
+
+let sex (p:Passenger) = p.Sex
+let sexClassifier = survived |> learn (dataset.Rows) sex
+
+dataset.Rows |> validate "passenger sex" sexClassifier
+
+let classFeature (p:Passenger) = p.Pclass
+let classClassifier = learn (dataset.Rows) classFeature survived
+
+dataset.Rows |> validate "passenger class" classClassifier
+
+dataset.Rows
+|> Seq.groupBy (fun p -> p.Fare)
+|> Seq.iter(fun (price, passengers) ->
+    printfn "%6.2F: %6.2f" price (survivalRate passengers))
+
+let averageFare =
+    dataset.Rows
+    |> Seq.averageBy (fun p -> p.Fare)
+let fareFeature (p:Passenger) =
+    if p.Fare < averageFare
+    then "Cheap"
+    else "Expensive"
+let fareClassifier = learn (dataset.Rows) fareFeature survived
+
+dataset.Rows |> validate "fare level" fareClassifier
+
+dataset.Rows
+|> Seq.groupBy (fun p -> p.Embarked)
+|> Seq.iter (fun (port, passengers) ->
+    printfn "%s: %f" port (survivalRate passengers))
+
+let port (p:Passenger) =
+    if p.Embarked = "" then None
+    else Some(p.Embarked) 
+let portClassifier = survived |> betterLearn (dataset.Rows) port
+
+dataset.Rows |> validate "embarked port" portClassifier
