e94d70aee4dcaaff70cfee0a9856c849ef6aa968
Michael Naumenko <agaspher@outlook.com>
2016-12-17 14:51:44 +0300

    Decision forest

---
+++ TreesAndForests/TreesAndForests/Forest.fs
@@ -0,0 +1,44 @@
+﻿module Forest
+    open System
+    open Tree
+
+    let pickRepeat (rng:Random) proportion original =
+        let size = original |> Array.length
+        let sampleSize = proportion * float size |> int
+        Array.init sampleSize (fun _ -> original.[rng.Next(size)])
+
+    let pickNoRepeat (rng:Random) proportion original =
+        let size = original |> Seq.length
+        let sampleSize = proportion * float size |> int
+        let init = ([],size)
+        original
+        |> Seq.fold (fun (sampled,remaining) item ->
+            let picked = sampled |> List.length
+            let p = float (sampleSize - picked) / float remaining
+            if (rng.NextDouble () <= p)
+            then (item::sampled,remaining-1)
+            else (sampled,remaining-1)) init
+        |> fst
+
+    let predict forest observation =
+        forest
+        |> Seq.map (fun tree -> decide tree observation)
+        |> Seq.countBy id
+        |> Seq.maxBy snd
+        |> fst
+
+    let growForest size sample label features =
+        let rng = Random ()
+        let propFeatures =
+            let total = features |> Seq.length |> float
+            sqrt total / total 
+        let featSample () = pickNoRepeat rng propFeatures features
+        let popSample () = pickRepeat rng 1.0 sample
+        let filters = [ leafSizeFilter 10; entropyGainFilter ] 
+        let forest = [
+            for _ in 1 .. size ->
+                let sample = popSample ()
+                let features = featSample () |> Map.ofList
+                growTree filters sample label features ]
+        let predictor = predict forest
+        predictor
---
+++ TreesAndForests/TreesAndForests/ForestDecision.fsx
@@ -0,0 +1,45 @@
+﻿#I @"..\packages\"
+#r @"FSharp.Data.2.3.2\lib\net40\FSharp.Data.dll"
+#load "DecisionStump.fs"
+#load "Tree.fs"
+#load "Forest.fs"
+#load "KFold.fs"
+
+open DecisionStump
+open Tree
+open Forest
+open KFold
+open FSharp.Data
+open System
+
+[<Literal>]
+let dataPath = __SOURCE_DIRECTORY__ + @"..\..\data\titanic.csv"
+
+type Titanic = CsvProvider<dataPath>
+type Passenger = Titanic.Row
+
+let dataset = Titanic.GetSample ()
+
+let label (p:Passenger) = p.Survived
+let forestFeatures = [
+    "Sex", fun (p:Passenger) -> p.Sex |> Some
+    "Class", fun p -> p.Pclass |> string |> Some
+    "Age", fun p -> if p.Age < 7.0 then Some("Younger") else Some("Older")
+    "Port", fun p -> if p.Embarked = "" then None else Some(p.Embarked) ]
+
+let forestResults () =
+    let folds =
+        dataset.Rows
+        |> Seq.toArray
+        |> kfold 10 
+    let accuracy predictor (sample:Passenger seq) =
+        sample
+        |> Seq.averageBy (fun p ->
+            if p.Survived = predictor p then 1.0 else 0.0) 
+    [for (training,validation) in folds ->
+        let forest = growForest 1000 training label forestFeatures
+        let accuracyTraining = accuracy forest training
+        let accuracyValidation = accuracy forest validation 
+        printfn "Training: %.3f, Validation: %.3f" accuracyTraining accuracyValidation
+        accuracyTraining,accuracyValidation ]
+forestResults ()
