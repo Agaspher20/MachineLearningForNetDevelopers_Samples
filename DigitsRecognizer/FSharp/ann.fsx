5c7d56eb94be5d9dac2d6d78c2ff97724f20f25a
Michael Naumenko <agaspher@outlook.com>
2017-01-09 11:11:43 +0300

    Recognizing digits with Accord.Neuro

---
+++ DigitsRecognizer/FSharp/ann.fsx
@@ -0,0 +1,50 @@
+﻿#I @"..\packages\"
+#r @"Accord.3.3.0\lib\net45\Accord.dll"
+#r @"Accord.Math.3.3.0\lib\net45\Accord.Math.dll"
+#r @"Accord.Neuro.3.3.0\lib\net45\Accord.Neuro.dll"
+#r @"Accord.Statistics.3.3.0\lib\net45\Accord.Statistics.dll"
+
+open Accord.Statistics
+open Accord.Neuro
+open Accord.Neuro.Learning
+
+let nnRead fileName =
+    let path = __SOURCE_DIRECTORY__ + @"..\..\data\" + fileName
+    path
+    |> System.IO.File.ReadAllLines
+    |> fun lines -> lines.[1..]
+    |> Array.map (fun line ->
+        let parsed = line.Split ','
+        parsed.[0] |> int, parsed.[1..] |> Array.map float)
+
+let trainNetwork (epochs:int) =
+    let features = 28*28
+    let labels,images = nnRead "trainingsample.csv" |> Array.unzip
+    let learningLabels = Tools.Expand(labels, -1.0, 1.0)
+    let network = ActivationNetwork(BipolarSigmoidFunction(), features, [| 100; 10 |])
+
+    NguyenWidrow(network).Randomize()
+
+    let teacher = new ParallelResilientBackpropagationLearning(network)
+    let rec learn iter =
+        let error = teacher.RunEpoch(images, learningLabels)
+        printfn "%.3f / %i" error iter
+        if error < 0.01 then ignore()
+        elif iter > epochs then ignore()
+        else learn (iter + 1)
+
+    learn 0
+
+    network
+
+let ann = trainNetwork(50)
+let validate = nnRead "validationSample.csv"
+
+validate
+|> Array.averageBy (fun (label, image) ->
+    let predicted =
+        ann.Compute image
+        |> Array.mapi (fun i x -> i,x)
+        |> Array.maxBy snd
+        |> fst
+    if label = predicted then 1.0 else 0.0)
